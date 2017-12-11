04385119500ca84542fefcec7401e9d9f2ac631f
Chris Nelson <chris@gaslight.co>
2017-12-11 11:33:23 -0500

    a little refactorin

--- src/container.js
+++ src/container.js
@@ -1,14 +1,11 @@
 document.querySelector("route-form").addEventListener("routeSelected", ({ detail: route }) => {
   console.log(route);
-  fetch(`http://localhost:5000/airport-lookup/api/ap_info.json?icao=${route.from}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const fullRoute = { from: data};
-      fetch(`http://localhost:5000/airport-lookup/api/ap_info.json?icao=${route.to}`)
-        .then((response) => response.json())
-        .then((data) => {
-          fullRoute.to = data;
-          document.querySelector("flight-nav").setAttribute("route", JSON.stringify(fullRoute));
-        });
-    });
+  const fromQuery = fetch(`http://localhost:5000/airport-lookup/api/ap_info.json?icao=${route.from}`)
+    .then((response) => response.json());
+  const toQuery = fetch(`http://localhost:5000/airport-lookup/api/ap_info.json?icao=${route.to}`)
+    .then((response) => response.json());
+  Promise.all([fromQuery, toQuery]).then(([from, to]) => {
+    console.log(from, to);
+    document.querySelector("flight-nav").setAttribute("route", JSON.stringify({from, to}));
+  });
 })
